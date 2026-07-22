0d98a553f16df3c87a769cdf942398c9155ff47f
melnik-av <andreyname@gmail.com>
2026-07-22 22:45:32 +0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 // ⚠️ ЗАМЕНИТЕ НА ВАШИ ДАННЫЕ ИЗ ШАГА 5
-const SUPABASE_URL = 'https://hpisnwhoqzjkcmmlkmys.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://hpisnwhoqzjkcmmlkmys.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwaXNud2hvcXpqa2NtbWxrbXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MTU5ODIsImV4cCI6MjEwMDI5MTk4Mn0.qDsx5Hx7OioDSNSYIuyXkM7KAraBYV0cluOHciGRwY8';
 
 // Инициализация клиента
